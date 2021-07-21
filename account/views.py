425dcd7832a3d05be91806c0a53881836d1c20e5
이승백 <yahoo557@naver.com>
2021-07-22 05:34:55 +0900

    로그인/로그아웃 구현

--- account/views.py
+++ account/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def login_views(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        id = request.POST['user-id']
+        password = request.POST['user-password']
+        user = authenticate(request = request, username = id, password = password)
+        if user is not None :
+            login(request, user)
+        return redirect('main')
+    else :
+        return render(request, 'login.html', {'error' : '일치하는 ID가 없어나 잘못된 Password 입니다.' })
+    
+
+
 
 def logout_views(request):
-    return render(request,'login.html')
+    logout(request)
+    return redirect('main')
 
 def register(request):
     return render(request,'register.html')
