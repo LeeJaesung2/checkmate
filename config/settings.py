0d06995272df74c6fe933901732115ee9c47f966
ooskdud <ooskdud@gmail.com>
2021-07-22 04:05:51 +0900

    css시작

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
