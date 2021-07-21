8816be7e0c0b405641b3eb3376b964e47a92dd9b
LeeJaesung2 <dlwotjd9909@naver.com>
2021-07-21 22:04:05 +0900

    accountapp

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_views(request):
+    return render(request,'login.html')
+
+def logout_views(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
