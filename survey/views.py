54e51e216104d026097d34b745b3e25357f892c8
doyeon <do8343@naver.com>
2021-07-26 00:29:29 +0900

    views 작업중

--- survey/views.py
+++ survey/views.py
@@ -6,3 +6,13 @@ from .models import SurveyEssential, SurveyOptional
 
 def survey(request):
     return render(request, 'survey.html')
+
+def createEssentialSurvey(request):
+    eSurvey = SurveyEssential() #EssentialSurvey
+    eSurvey.grade = request.GET['grade']
+    eSurvey.room_type = request.GET['room-type']
+    eSurvey.dormitory_number = request.GET['dormitory-number']
+    eSurvey.dormitory_year_start = request.GET['dormitory-year-start']
+    print(request.GET['room-type'])
+
+    return render(request, 'survey.html')
