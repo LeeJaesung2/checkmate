8b83d9e7e6cddb3fc69d53f5676904e6e87e91b7
LeeJaesung2 <dlwotjd9909@naver.com>
2021-07-26 20:19:38 +0900

    카카오 로그인

--- account/views.py
+++ account/views.py
@@ -27,34 +27,36 @@ def logout_view(request):
     return redirect('main')
 
 def register(request):
-
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+          #  user = form.save(commit=False)
+          #  user.email =
+          #  user.save()
             login(request,user)
         return redirect('main')
     else:
         form = RegisterForm()
-        return render(request, 'register.html', {'form':form})
+        return render(request, 'register.html',{'form':form})
 
 
 def kakaoLoginLogic(request):
-    _restApiKey = '	f9d119f1d082709b083b6ee7e2333682'
+    _restApiKey = 'f9d119f1d082709b083b6ee7e2333682'
     _redirectUrl = 'http://127.0.0.1:8000/account/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
     return redirect(_url)
 
 def kakaoLoginLogicRedirect(request):
     _qs = request.GET['code']
-    _restApiKey = '	f9d119f1d082709b083b6ee7e2333682'
+    _restApiKey = 'f9d119f1d082709b083b6ee7e2333682'
     _redirect_uri = 'http://127.0.0.1:8000/account/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
     _res = requests.post(_url)
     _result = _res.json()
     request.session['access_token'] = _result['access_token']
     request.session.modified = True
-    return render(request, 'main.html')
+    return redirect('register')
 
 def kakaoLogout(request):
     _token = request.session['access_token']
@@ -62,14 +64,10 @@ def kakaoLogout(request):
     _header = {
       'Authorization': f'bearer {_token}'
     }
-    # _url = 'https://kapi.kakao.com/v1/user/unlink'
-    # _header = {
-    #   'Authorization': f'bearer {_token}',
-    # }
     _res = requests.post(_url, headers=_header)
     _result = _res.json()
     if _result.get('id'):
         del request.session['access_token']
         return render(request, 'main.html')
     else:
-        return render(request, 'register.html')
+        return render(request, 'login.html')
--- checkmate/views.py
+++ checkmate/views.py
@@ -3,17 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    _context = {'check':False}
-    if request.session.get('access_token'):
-        _context['check'] = True
-    return render(request,'main.html', _context)
-
-
-# def login(request):
-#     return render(request,'login.html')
-    
-# def register(request):
-#     return render(request,'register.html')
+    return render(request,'main.html')
 
 def infoWrite(request):
     return render(request, 'infoWrite.html')
