b0ea303330b9830994ddd411671bd0eb8ee485f1
이승백 <yahoo557@naver.com>
2021-08-02 12:45:51 +0900

    비밀번호 변경 고침, 에러메세지 출력

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
 from .models import CustomUser
 
 
@@ -7,4 +7,3 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields+('user_gender','user_nation','user_nickname','kakao_id')
-
--- account/views.py
+++ account/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.forms import AuthenticationForm
 import requests
 import json
 from django.contrib import auth
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login, logout,update_session_auth_hash
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterForm
 from django.contrib.auth.hashers import check_password
+from django.contrib import messages
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -17,14 +18,19 @@ def login_view(request):
             user = authenticate(request=request, username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-        return redirect('main')
+            return redirect('main')
+        else :
+            messages.warning(request, '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.')
+            return render(request, 'login.html', {'form':form})
     else:
+        
         form = AuthenticationForm()
         return render(request, 'login.html', {'form':form})
 
 
 def logout_view(request):
     logout(request)
+    
     return redirect('main')
 
 def register(request):
@@ -45,9 +51,9 @@ def register(request):
         return render(request, 'register.html',{'form':form, 'kakao_id':kakao_id})
 
 
+
 def mypageProfile(request):
-    context = {}
-    if request.method == 'POST':
+    if request.method == "POST":
         current_password = request.POST.get('user_password')
         user = request.user
         if check_password(current_password,user.password):
@@ -59,10 +65,12 @@ def mypageProfile(request):
                 login(request,user)
                 return redirect('main')
             else:
-                context.update({'error' : "새로입력한 비밀번호가 일치하지 않습니다."})
+                messages.warning(request, '새로 입력한 비밀번호가 서로 일치하지 않습니다.')
         else:
-            context.update({'error' : "현재 비밀번호가 일치하지 않습니다."})
-    return render(request, 'mypageProfile.html', context)
+            messages.warning(request, '현재 비밀번호가 일치하지 않습니다.')
+
+    return render(request, 'mypageProfile.html')
+
 
 
 
@@ -83,20 +91,3 @@ def kakaoLoginLogicRedirect(request):
     request.session.modified = True
     return redirect('register')
 
-# def kakaoLogout(request):
-#     _token = request.session['access_token']
-#     _url = 'https://kapi.kakao.com/v1/user/logout'
-#     _header = {
-#       'Authorization': f'bearer {_token}'
-#     }
-#     _res = requests.post(_url, headers=_header)
-#     _result = _res.json()
-#     if _result.get('id'):
-#         del request.session['access_token']
-#         return render(request, 'main.html')
-#     else:
-#         return render(request, 'login.html')
-
-
-def mypageProfile(request):
-    return render(request, 'mypageProfile.html')
