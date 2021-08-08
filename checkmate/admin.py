e0c7a46823c683fd7e1d54013b3e16854afd18c8
LeeJaesung2 <dlwotjd9909@naver.com>
2021-08-09 02:38:35 +0900

    scrap 완료X

--- checkmate/admin.py
+++ checkmate/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from checkmate.models import Offcampus_Post,Domitory_Post
+from checkmate.models import Offcampus_Post,Domitory_Post, Scrap
 
 # Register your models here.
 admin.site.register(Offcampus_Post)
 admin.site.register(Domitory_Post)
+admin.site.register(Scrap)
